4651aaf671c36a878b19154ab18fb45f8bca959c
Timofey <59933599+tima-bit@users.noreply.github.com>
2023-07-03 22:50:50 +0300

    Create arduino_start_rockets_test_oled.v2.ino
    add oled

---
+++ arduino_start_rockets_test_oled.v2.ino
@@ -0,0 +1,140 @@
+#include <GyverOLED.h>
+
+GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+
+String Captcha;
+String plug;
+ 
+void setup() {
+  oled.init();        // инициализация
+  oled.clear();       // очистка
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  Captcha = String(random(10)) + String(random(10)) + String(random(10)) + String(random(10));
+  Serial.println("Captcha: " + Captcha);
+  oled.setScale(1);   // масштаб текста (1..4)
+  oled.setCursorXY(2, 25);
+  oled.print("Captcha: " + Captcha);
+}
+
+void loop() {
+    if (Serial.available() > 0){
+      String id_data = Serial.readString();
+      Captcha_func(id_data);
+    }   
+}
+
+void Captcha_func(String number){
+  if (number == Captcha){
+    Serial.println("Authorization was successful. Console yes");
+    Serial.println("Write \"plugdi\" activates commands, \"plugen\" prohibits entering commands");
+    Serial.println("Write \"start\" to start the launch(you need to enable \"plugdi\")");
+    Serial.println("The plug is enabled");
+    while(true){
+      if (Serial.available() > 0){
+      String id_data = Serial.readString();
+      if (id_data == "plugen"){
+        Serial.println("The plug is enabled");
+        plug = "yes";
+        }else if (id_data == "plugdi"){
+          Serial.println("The plug is disable");
+          plug = "no";
+          }
+      if (plug == "no"){
+        start(id_data);
+        }
+    }
+      }
+    }else{
+      Serial.println("Authorization failed. Console no (you need to put \"No Line Ending\"!)");
+      }
+  }
+
+void start(String lift_off){
+  String time_str;
+  Serial.println(lift_off);
+  if (lift_off == "start"){
+    Serial.println("Command accepted. Please move away, very dangerous !!!");
+    Serial.println("Countdown:");
+    delay(5000);
+    for (int time = 10; time >= -1; time--){
+      if (time > 0){
+          delay(1000);
+          if (time < 10){
+            time_str = "0" + String(time);
+          }else{
+            time_str = time;
+          }
+          Serial.println("T - 00:00:" + time_str);
+          oled.setScale(2);   // масштаб текста (1..4)
+          oled.setCursorXY(2, 25);
+          oled.print("T-00:00:" + time_str);
+        }
+     
+        else if(time == 0){
+          delay(1000);
+          Serial.println("T + 00:00:00");
+          oled.setScale(2);   // масштаб текста (1..4)
+          oled.setCursorXY(2, 25);
+          oled.print("T-00:00:" + time_str);
+          status_start_ok();
+        }
+    }
+  }    
+  else{
+      Serial.println("Launch not allowed!");
+      }
+  }
+
+void status_start_ok(){
+  int seconds=0;
+  int minute=0;
+  int hours=0;
+  String seconds_str;
+  String minute_str;
+  String hours_str;
+  while(true){
+    delay(1000);
+    seconds++;
+    if (seconds < 10){
+            seconds_str = "0" + String(seconds);
+          }else{
+            seconds_str = seconds;
+          }
+    if (minute < 10){
+            minute_str = "0" + String(minute);
+          }else{
+            minute_str = minute;
+          }
+    if (hours < 10){
+            hours_str = "0" + String(hours);
+          }else{
+            hours_str = hours;
+          }
+    Serial.println("T + " + hours_str + ":" + minute_str + ":" + seconds_str );
+    oled.setScale(2);   // масштаб текста (1..4)
+    oled.setCursorXY(2, 25);
+    oled.print("T+" + hours_str + ":" + minute_str + ":" + seconds_str );
+    if (seconds == 59){
+      minute++;
+      seconds = -1;
+    }
+    if (minute == 59){
+      hours++;
+      minute = -1;
+    }
+     if (Serial.available() > 0){
+      String id_data = Serial.readString();
+      if (id_data == "stop"){
+        Serial.println("stop");
+        Serial.println("Captcha: " + Captcha);
+        while(true){
+        if (Serial.available() > 0){
+          String id_data = Serial.readString();
+          Captcha_func(id_data);
+          }
+        }
+        }
+    }
+    }
+  }
